b3f59418bcda5d7a3e6e6e230db615a980613b15
yangyb <1173086039@qq.com>
2020-12-21 19:08:57 +0800

    初始化

---
+++ project/docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    title: '个人主页', 
+    description: '杨一博的博客',
+    head: [
+        ['link', { rel: 'icon', href: '/img/logo.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ],
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '博文',
+              items: [
+                { text: 'Android', link: '/android/' },
+                { text: 'ios', link: '/ios/' },
+                { text: 'Web', link: '/web/' }
+              ] 
+            },
+            { text: '关于', link: '/about/' },
+            { text: 'Github', link: 'https://www.github.com/codeteenager' },
+        ],
+        sidebar: {
+            '/android/': [
+                        "",
+                        "android1"
+                         ],
+                "/ios/":[
+                        "",
+                        "ios1"
+                        ],
+                "/web/":[
+                        "",
+                        "web1"
+                             ],
+            },
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated', 
+    },
+    serviceWorker: true,
+}
---
+++ project/docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /favicon.ico
+actionText: 快速上手 →
+actionLink: /guide/
+features:
+- title: 简洁至上
+  details: xxxxxxxxxxxxxxxxxx。
+- title: Vue驱动
+  details: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx。
+- title: 高性能
+  details: xxxxxxxxxxxxxxxxxxxxxxxxx。
+footer: MIT Licensed | Copyright © 2018-present xxxxxx
+---
